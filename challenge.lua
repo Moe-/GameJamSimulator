6f3abd2c47540c72448a8b32c3e03324a906c461
Moe <moe@diccuric.org>
2015-03-14 12:25:20 +0100

    Add challenges

---
+++ challenge.lua
@@ -0,0 +1,21 @@
+class "Challenge" {
+  posx = 0;
+  posy = 0;
+}
+
+function Challenge:__init(posx, posy)
+  self.posx = posx
+  self.posy = posy
+  self.image = love.graphics.newImage("gfx/challenge.png")
+  self.quad = love.graphics.newQuad(0, 0, 2048, 2048, self.image:getWidth(), self.image:getHeight())
+  self.width = self.image:getWidth()
+  self.height = self.image:getHeight()
+end
+
+function Challenge:update(dt)
+  
+end
+
+function Challenge:draw()
+  love.graphics.draw(self.image, self.quad, self.posx, self.posy)
+end
--- world.lua
+++ world.lua
@@ -1,9 +1,11 @@
 require('background')
 require('player')
+require('challenge')
 
 class "World" {
   width = 0;
   height = 0;
+  challengeCount = 10;
 }
 
 function World:__init(width, height)
@@ -11,16 +13,34 @@ function World:__init(width, height)
   self.height = height
   self.background = Background:new()
   self.player = Player:new(200, 200)
+  self.challenges = {}
+  for i = 1, self.challengeCount do
+    self:genChallenge()
+  end
+end
+
+function World:genChallenge()
+  local x = math.random(1, self.width)
+  local y = math.random(1, self.height)
+  table.insert(self.challenges, Challenge:new(x, y))
 end
 
 function World:update(dt)
   self.background:update(dt)
   self.player:update(dt)
+  
+  for i, v in pairs(self.challenges) do
+    v:update(dt)
+  end
 end
 
 function World:draw()
   self.background:draw()
   self.player:draw()
+  
+  for i, v in pairs(self.challenges) do
+    v:draw()
+  end
 end
 
 function World:keypressed(key)
